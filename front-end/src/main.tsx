db77b28152265205066c8b42e9784e96679702b3
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-07-09 19:08:04 +0300

    ready

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
@@ -8,13 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId="98978859292-8ver762isock1hdddvkeu8ibmmn5908h.apps.googleusercontent.com">
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </GoogleOAuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <GoogleOAuthProvider clientId="98978859292-8ver762isock1hdddvkeu8ibmmn5908h.apps.googleusercontent.com">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </GoogleOAuthProvider>
+  </BrowserRouter>
 );
